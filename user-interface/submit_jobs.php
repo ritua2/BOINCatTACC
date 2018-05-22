fe6e1f50b9c8caf183888fbcc7b1efa1e6397d66
Carlos Redondo <carlos.red@utexas.edu>
2018-05-22 18:08:54 -0500

    Fixed submission, again

--- user-interface/submit_jobs.php
+++ user-interface/submit_jobs.php
@@ -7,22 +7,22 @@
 
   <body>
 
+         <style>
+         div {
+             background-color: DodgerBlue;
+             width: 350px;
+             padding: 25px;
+             margin: 25px;
+          }
+          </style>
+          
+          <div>
+          <h3>Interactive BOINC web interface</h3>
+          </div>
 
-    <style>
-     div {
-         background-color: DodgerBlue;
-         width: 350px;
-         padding: 25px;
-         margin: 25px;
-      }
-      </style>
-
-      <div>
-      <h3>Interactive BOINC web interface</h3>
-    </div>
 
     <p>
-      </h4>Submit jobs via boinc2docker.</h4>
+           </h4> Submit jobs via boinc2docker.</h4>
     </p>
           
 
@@ -32,21 +32,17 @@
 
          <!-- Token input -->
    <form name="form" action="proc_submit.php" method="post">
-   <p>Enter token: </p>
-   <input type="text" name="TOK" id="TOK" value="">
+          
+    Token: <input type="text" name="TOK" id="TOK" value=""><br>
+    Command: <input type="text" name="DERS" id="DERS" value=""><br>
+    <input type = "submit" value = "Submit">
    </form>
 
-   <!-- Command input -->
-   <form name="form" action="proc_submit.php" method="post">
-   <p>Enter the command without bin/boinc2docker_create_work.py: </p>
-   <input type="text" name="DERS" id="DERS" value="">
-   </form>
-   
-   <p><br><br>
-   <p>
+    <p><br><br>
+    <p>
 
    Program is not designed for high API usage, for more information batch submission using RCP APIs, check the official BOINC documentation.
-   </p>
+    </p>
 
 
   </body>

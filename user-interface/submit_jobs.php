51c8c2e28102131503b57fc0aeca6725bbcc9b93
Carlos Redondo <carlos.red@utexas.edu>
2018-05-22 16:42:37 -0500

    Changed basic submit page

--- user-interface/submit_jobs.php
+++ user-interface/submit_jobs.php
@@ -1,28 +1,33 @@
 <!DOCTYPE html>
 
  <html>
- 	<head>
- 		<title>Submit BOINC jobs</title>
- 	</head>
+  <head>
+    <title>Submit BOINC jobs</title>
+  </head>
 
-	<body>
-	  <p>
-	   <h3>Interactive BOINC web interface.</h3>
-           </h4>Submit jobs via boinc2docker.</h4>
-	  </p>
+  <body>
+    <p>
+     <h3>Interactive BOINC web interface.</h3>
+      </h4>Submit jobs via boinc2docker.</h4>
+    </p>
           
 
-          <p><br><br>
-          WARNING: Do not press ENTER until all fields have been completed.
-	         
+    <p><br><br>
+    WARNING: Do not press ENTER until all fields have been completed.
+           
 
          <!-- Token input -->
-	 <form name="form" action="proc_submit.php" method="post">
-          <p>Enter token: </p>
-          <input type="text" name="TOK" id="TOK" value="">
-          </form>
+   <form name="form" action="proc_submit.php" method="post">
+   <p>Enter token: </p>
+   <input type="text" name="TOK" id="TOK" value="">
+   </form>
 
-          <!-- Command input -->
+   <!-- Command input -->
+   <form name="form" action="proc_submit.php" method="post">
+   <p>Enter the command without bin/boinc2docker_create_work.py: </p>
+   <input type="text" name="DERS" id="DERS" value="">
+   </form>
 
-	</body>
+
+  </body>
 </html> 

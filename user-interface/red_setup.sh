e4d0956ff7282bad71b67068b36a5860f974505a
Carlos Redondo <carlos.red@utexas.edu>
2018-05-24 15:41:37 -0500

    Setup for a basic Redis database for stored jobs

---
+++ user-interface/red_setup.sh
@@ -0,0 +1,16 @@
+#!bin/bash
+
+############
+# BASICS
+#
+# Necessary packages to set-up and work with a Redis database to store job submission
+############
+
+# Sets up a Redis client on port 6389
+
+apt-get update -y
+apt-get install redis-server -y
+# Sets up a redis server on port 6389, which must be open in the docker-compose.yml
+redis-server --port 6389 &
+# Sets up python3, needed
+apt-get install python3 python3-pip -y

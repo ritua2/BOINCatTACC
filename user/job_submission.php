41ad89634a4f0b96c77003e77519d1078b47849b
Gerald Joshua <gerald.joshua153@utexas.edu>
2018-07-18 14:11:54 -0500

    The working version of job_submission.php and job_submission_result.php, edited by Gerald Joshua

--- user/job_submission.php
+++ user/job_submission.php
@@ -35,6 +35,7 @@ echo '<center><h1>Job Submission</h1></center><br />';
 
 //Added by Gerald Joshua
 echo '
+	<form action = "http://129.114.16.64/boincserver/job_submission_result.php" method = "post" enctype="multipart/form-data">
 	<span style="margin-left: 17.3%; float:left; font-weight:bold;"><a data-toggle="tooltip" style ="border-bottom: 1px dotted #000; text-decoration: none;" title="The location of docker image that will be used can be from docker hub or from the list of available tacc-2-boinc docker images">'.tra("Location of docker image").'</a></span>
 	<div style="float:left; margin-left: 3.5%;"><label style="margin-left: 3px;"><input type="radio" id="dockerOpt1" checked="checked"><span style="margin-left: 5px;">List of TACC-2-BOINC docker images</span></label></div>
 	<div style="float:left; margin-left: 4%;"><label><input type="radio" id="dockerOpt2"><span style="margin-left: 5px;">Docker hub</span></label></div>
@@ -43,16 +44,17 @@ echo '
 	<div class="dropdown" style="margin-left: 34.5%;" >
   		<button class="btn btn-primary dropdown-toggle" id="dockerListBtn" value="none"  style="background-color: #174b63; font-weight: bold;" type="button" data-toggle="dropdown"><span id="buttonText">List of TACC-2-BOINC docker images</span>
   		<span class="caret"></span></button>
+		<input type="hidden" id="dockerListParam" name="dockerList" value="none">
   		<ul class="dropdown-menu">
-    			<li><a href="javascript:;">AutoDock Vina</a></li>
-    			<li><a href="javascript:;">OpenSees</a></li>
-    			<li><a href="javascript:;">BLAST</a></li>
-			<li><a href="javascript:;">Bedtools</a></li>
- 			<li><a href="javascript:;">HTSeq</a></li>
-			<li><a href="javascript:;">Gromacs</a></li>
-			<li><a href="javascript:;">LAMMPS</a></li>
-			<li><a href="javascript:;">NAMD</a></li>
-			<li><a href="javascript:;">Bowtie</a></li> 		
+    			<li value="carlosred/autodock-vina"><a href="javascript:;">AutoDock Vina</a></li>
+    			<li value="carlosred/opensees"><a href="javascript:;">OpenSees</a></li>
+    			<li value="carlosred/blast"><a href="javascript:;">BLAST</a></li>
+			<li value="carlosred/bedtools"><a href="javascript:;">Bedtools</a></li>
+ 			<li value="carlosred/htseq"><a href="javascript:;">HTSeq</a></li>
+			<li value="carlosred/gromacs"><a href="javascript:;">Gromacs</a></li>
+			<li value="carlosred/mpi-lammps"><a href="javascript:;">LAMMPS</a></li>
+			<li value="carlosred/namd-cpu"><a href="javascript:;">NAMD</a></li>
+			<li value="carlosred/bowtie"><a href="javascript:;">Bowtie</a></li> 		
 		</ul>
 	</div><br />
 	</div>
@@ -66,12 +68,13 @@ form_input_text(/*Commented out by Gerald Joshua
             '<!-- attribute href of html tag a was removed by Gerald Joshua --><a data-toggle="tooltip" style="border-bottom: 1px dotted #000;text-decoration: none;" 
             title="The name of the Dockerhub Image that will be utilized for this job submission. Provide the name, not the URL.">'.tra("Docker Hub Image").'</a>'
         ) End of Gerald Joshua's edit*/ "" ,
-        "Image", "", "",/*Added by Gerald Joshua */"style='margin-bottom: 15px;' id='dockerHubFile' placeholder='e.g., tacc/docker2singularity (provide the name, not the URL)'"
+        "Image", "", "",/*Added by Gerald Joshua */"style='margin-bottom: 15px;' name='dockerFileName' id='dockerHubFile' placeholder='e.g., tacc/docker2singularity or nginx:1.14.0 (provide the name, not the URL)' value='none'"
 /*End of Gerald Joshua's edit*/);
 
 //Added by Gerald Joshua
 echo "
-	<input class='btn btn-success' id='checkBtn' value='Check if exists' style='font-weight: bold; margin-left: 34.5%;'>
+	<button type='button' class='btn btn-success' id='checkBtn' style='font-weight: bold; margin-left: 34.5%;' onclick='checkIfExists();'>Check if it exists on docker hub</button>
+	<div id='transparentGreyArea' style='background-color: grey; opacity: 0.5; filter: alpha(opacity=50); height: 100vh; width: 100%; z-index:9998;position: fixed; top: 0; left: 0;;'><i class='fa fa-spinner fa-spin' style='position: fixed; left: 50%; top: 50%; z-index:9999; font-size:60px'></i></div><span id='dockerNotification' style='margin-left: 10px; font-weight: bold;'></span>
 	<br /><br /></div>";
 //End of Gerald Joshua's edit
 
@@ -93,7 +96,7 @@ form_input_text(sprintf('<!-- style of span was added by Gerald Joshua--><span s
 //Section for commands
 echo '<span style="margin-left: 21.3%; font-weight:bold;"><a data-toggle="tooltip" style ="border-bottom: 1px dotted #000; text-decoration: none;" title="The list of commands that will be used in processing the data. Must provide all necessary commands.">'.tra("List of commands").'</a></span>';
 
-echo '<textarea id="commandLines" style="margin-top: -20px;margin-left: 34.5%;width: 64.5%;padding: 10px; border-radius: 5px;" rows="7" placeholder="e.g., gcc -o hello.exe hello.c (hit enter at each of the end the command line)"></textarea><br /><br />';
+echo '<textarea id="commandLines" name="theCommandLine" style="margin-top: -20px;margin-left: 34.5%;width: 64.5%;padding: 10px; border-radius: 5px;" rows="7" placeholder="e.g., gcc -o hello.exe hello.c (hit enter at each of the end the command line including the last command line)"></textarea><br /><br />';
 tra("Please upload your relevant tar (.tgz or .tar.gz extensions only) or zip (.zip extenstion only) for the current submission.");
 //End of commands section
 
@@ -109,17 +112,19 @@ echo '<span style="margin-left: 26%; float:left; font-weight:bold;"><a data-togg
 
 <label style="margin-left: 30px;"><input type="radio" id="zipUpload"><span style="margin-left: 5px;">Zip Upload</span></label><br />
 
+  <div style="margin-left:34.3%;">
 <!-- Added by Gerald Joshua, sample code for form tag from Thomas -->
-<form action = "job_submission_result.php" style="margin-left: 34.5%;" method = "post" enctype="multipart/form-data">
     <label class="btn btn-success" style="font-weight: bold; margin-bottom: 5px;">
-    Browse <input type="file" name="fileToUpload" id="uploadBtn" onchange="fileExtensionChecking(this);"> 
+    Browse <input type="file" name="file" id="uploadBtn" onchange="fileExtensionChecking(this);"> 
     </label>
     <span class="label label-info" id="fileLocation" style="font-size: 14px;">No file chosen</span> 
     <br /><span id="warningMsg" style="color: red; font-weight: bold;"><br /></span><br />
-<input class="btn btn-success" type="submit" id="submitBtn" value="Submit the job" style="font-weight: bold;">
+    <input type="hidden" id="theParameters" value="" />
+    <input class="btn btn-success" type="submit" id="submitBtn" value="Submit the job" style="font-weight: bold;">
+  </div>
 </form>';
 
-echo "<input class='btn btn-success' value='test'  onclick='testFunction();'>";
+echo '<span id="testBtn"></span>';
 
 
 //End of Thomas Johnson's edit
@@ -132,22 +137,73 @@ echo'
 	var isDockerChosen = false;
 	var isCommandInputed = false;
 	var isFileUploaded = false;
-
-	function testFunction(){
-		$.ajax({
-  			type: "POST",
-  url: "http://129.114.16.64/boincserver/some.php",
-  data: { name: "John" },
-  success: function(result){
-	console.log(result);
-}})  	
-	}
+	var lastDockerImgInputed = "";		
 
 	//Submit button will be available if all fields are filled out
 	function activateSubmitBtn(taccDocker, command, file){
 		if (taccDocker && command && file){
 			$("#submitBtn").prop("disabled",false);
 		}
+	}
+
+	//Check if docker image inserted by the user exists in docker hub
+	function checkIfExists(){
+		$(".fa-spinner").show();
+		$("#transparentGreyArea").show();
+		var dockerImgName = $("#dockerHubFile").val();		
+		var dockerImgFound = false;				
+		lastDockerImgInputed = dockerImgName;		
+
+		//Find if docker image has a tag or not
+		if(dockerImgName.indexOf(":") > -1){//Colon exists
+			$.ajax({
+  				type: "POST",
+  				url: "http://129.114.16.64/checkIfExists.php",
+  				data: { 
+					tagExists: "1",
+					tag: String(dockerImgName.split(":")[1])					,dockerName: String(dockerImgName.split(":")[0])
+				},
+  				success: function(result){
+					console.log(result);
+					dockerImgFound = result;
+					isDockerChosen = dockerImgFound;
+					if(isDockerChosen){
+						activateSubmitBtn(isDockerChosen, isCommandInputed, isFileUploaded);
+					$("#dockerNotification").text("The docker image exists on docker hub");
+					}
+					else{
+						$("#submitBtn").prop("disabled",true);
+						$("#dockerNotification").text("The docker image DOES NOT exist on docker hub");
+					}
+					$(".fa-spinner").hide();
+					$("#transparentGreyArea").hide();
+				}			
+			});
+		} else {//No tag exists
+			$.ajax({
+  				type: "POST",
+  				url: "http://129.114.16.64/checkIfExists.php",
+  				data: { 
+					tagExists: "0",
+					dockerName: dockerImgName
+				},
+  				success: function(result){
+						console.log(result);
+						dockerImgFound = result;
+						isDockerChosen = dockerImgFound;
+						if(isDockerChosen){
+						activateSubmitBtn(isDockerChosen, isCommandInputed, isFileUploaded);
+					$("#dockerNotification").text("The docker image exists on docker hub");
+					}
+					else{
+						$("#submitBtn").prop("disabled",true);
+						$("#dockerNotification").text("The docker image DOES NOT exist on docker hub");
+					}
+					$(".fa-spinner").hide();
+					$("#transparentGreyArea").hide();
+				}			
+			});
+		}			
 	}
 
 	//Function that handles file extension checking process
@@ -186,16 +242,18 @@ echo'
 		$("#uploadBtn").hide();
 		$("#warningMsg").hide();		
 		$("#submitBtn").prop("disabled",true);
-	
+		$(".fa-spinner").hide();
+		$("#transparentGreyArea").hide();
+
 		//Check whether the command lines have been inputed or not
 		$("#commandLines").bind("input propertychange", function() {
   			if(this.value.length > 0){
 				isCommandInputed = true;
-				activateSubmitBtn(isDockerChosen, isCommandInputed, isFileUploaded);	
+				activateSubmitBtn(isDockerChosen, isCommandInputed, isFileUploaded);					
 			}
-			else {
-				isCommandInputed = false;
+			else{
 				$("#submitBtn").prop("disabled", true);
+				isCommandInputed = false;
 			}
 		});
 
@@ -207,21 +265,23 @@ echo'
 		});
 		
 		$("#dockerHubFile").bind("input propertychange", function() {
-			if(this.value.length > 0){
+			if(this.value != lastDockerImgInputed){
+				isDockerChosen = false;
+				$("#dockerNotification").text("");
+				$("#submitBtn").prop("disabled", true);	
+			}
+			else if(this.value == lastDockerImgInputed){
 				isDockerChosen = true;
 				activateSubmitBtn(isDockerChosen, isCommandInputed, isFileUploaded);
-			}
-			else {
-				isDockerChosen = false;
-				$("#submitBtn").prop("disabled", true);
 			}
 		});
 
 		//For docker images dropdown list 
 		$(".dropdown-menu li").click(function(){
 			$("#buttonText").text($(this).text());
-      			$("#dockerListBtn").val($(this).text());
-   			isDockerChosen = true;
+      			$("#dockerListBtn").val($(this).attr("value"));
+   			$("#dockerListParam").val($(this).attr("value"));
+			isDockerChosen = true;
 			activateSubmitBtn(isDockerChosen, isCommandInputed, isFileUploaded);
 		});
 		$("#dockerOpt1").click(function(){
@@ -231,6 +291,7 @@ echo'
 			$("#taccDockerSection").show();
 			$("#buttonText").text("List of TACC-2-BOINC docker images");
       			$("#dockerListBtn").val("none");
+			$("#dockerListParam").val("none");
 			isDockerChosen = false;
 			$("#submitBtn").prop("disabled",true);
 		});
@@ -241,6 +302,8 @@ echo'
 			$("#taccDockerSection").hide();
 			isDockerChosen = false;
 			$("#submitBtn").prop("disabled",true);
+			$("#dockerNotification").text("");
+			$("#dockerHubFile").val("");
 		});
 
 		//For files upload
@@ -259,61 +322,10 @@ echo'
 		$("#uploadBtn").on("change", function() {
 			theFileLocation = $(this).val();
   			$("#fileLocation").text(theFileLocation);
-		});
-		
-		/*$.ajax({
-			url: "https://index.docker.io/v1/repositories/tacc/docker2singularity/images",
-			//crossDomain: true,
-			success: function(response){
-				console.log("success");
-			}
-		});
-		$.ajax({
-
-  type: "POST",
-
-  // The URL to make the request to.
-  url: "https://index.docker.io/v1/repositories/tacc/docker2singularity/images",
-
-  xhrFields: {
-    withCredentials: false
-  },
-
-  headers: {
-	"Access-Control-Allow-Origin": "*"
-  },
-
-  success: function() {
-  	console.log("success");
-	},
-
-  error: function() {
-  }
-});*/
+		});	
 	});
 </script>
 ';
 page_tail();
-
-// Generated by curl-to-PHP: http://incarnate.github.io/curl-to-php/
-// Get the token based on the user's email address
-$ch = curl_init();
-
-curl_setopt($ch, CURLOPT_URL, "http://129.114.16.64:5054/boincserver/v2/api/authorize_from_org");
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($ch, CURLOPT_POSTFIELDS, "email=$user->email_addr&org_key=d3fw115lVInWgihpEIU1lBu8");
-curl_setopt($ch, CURLOPT_POST, 1);
-
-$headers = array();
-$headers[] = "Content-Type: application/x-www-form-urlencoded";
-curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-
-$result = curl_exec($ch);
-//echo "result: ".$result;
-if (curl_errno($ch)) {
-    echo 'Error:' . curl_error($ch);
-}
-curl_close ($ch);
-//End of getting user's token
 //End of Gerald Joshua's edit
 ?>

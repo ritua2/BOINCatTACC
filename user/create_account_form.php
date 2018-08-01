21537b43eda8b5b399c5477251455ad49fd63ac0
Gerald Joshua <gerald.joshua153@utexas.edu>
2018-08-01 15:19:48 -0500

    Small edits on UI and terms and agreements
    Co-authored-by: Thomas Johnson <thomash.johnson261@gmail.com>

--- user/create_account_form.php
+++ user/create_account_form.php
@@ -16,6 +16,8 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with BOINC.  If not, see <http://www.gnu.org/licenses/>.
 
+//Edited by Thomas Johnson III
+
 require_once('../inc/boinc_db.inc');
 require_once('../inc/util.inc');
 require_once('../inc/account.inc');
@@ -39,18 +41,30 @@ if (parse_bool($config, "no_web_account_creation")) {
 }
 
 page_head(
-    tra("Create account"), null, null, null, boinc_recaptcha_get_head_extra()
+    null, null, null, null, boinc_recaptcha_get_head_extra(), "Create Account"//Keeps the tab title as Create Account without making the tab title subject to changes in the <body>
 );
 
+echo '<meta name = "viewport" content = "width=device-width, initial-scale=1.0">';
+
+//'<div style ="position:relative; left:36%;"> Original div used for spacing, caused issues as the div always retains the same amount of area in a webpage
+echo '<a data-toggle="tooltip" style="border-bottom: 1px dotted #000;text-decoration: none; position:relative; left:36%;" 
+title="Those interested in supporting R&D activities of researchers 
+and scholars using TACC resources are invited to join the 
+TACC-2-BOINC project as volunteers and the job for volunteers are 
+to donate their spare compute-cycles to the TACC-2-BOINC projects.">
+<font size=+3><br />'.tra("Create a Volunteer Account").'</font></a>';//Repositions the `Create Account text on the webpage
+
+echo '<br>';// Keeps the text from running into each other
+
 if (!NO_COMPUTING) {
-    echo "<p class=\"lead\">"
+    echo '<font size=+1 style ="position:relative; left:25%;">'//Alters the font size and repositions the text
         .tra(
             "If you already have an account and want to run %1 on this computer, %2 go here %3.",
             PROJECT,
             '<a href=join.php>',
             '</a>'
         )
-        ."</p>
+        ."</font>
     ";
 }
 
@@ -66,15 +80,132 @@ if ($teamid) {
     }
 }
 
-form_start("create_account_action.php","post");
+echo '<br>';
+
+//Using the tool-tip script that Joshua utilized on index.php
+/*echo /*attribute href of html tag a was removed by Gerald Joshua*//*'<a data-toggle="tooltip" style="border-bottom: 1px dotted #000;text-decoration: none; position:relative; left:36%;" 
+title="Those interested in supporting R&D activities of researchers 
+and scholars using TACC resources are invited to join the 
+TACC-2-BOINC project as volunteers and the job for volunteers are 
+to donate their spare compute-cycles to the TACC-2-BOINC projects.">
+<font size=+1>'.tra("Become a Volunteer:").'</font></a>';//Establishes the `Become a Volunteer` Tooltip with associated information, font size was manipulated as wel as position
+*/
+echo '<br>';
+
+form_start("create_account_action.php","post");//Starts the form for information input for the Volunteer
 create_account_form($teamid, $next_url);
 if ($recaptcha_public_key) {
     form_general("", boinc_recaptcha_get_html($recaptcha_public_key));
 }
-form_submit(tra("Create account"));
+
+//http://www.phphaven.com/article.php?id=77
+//$agree;
+
+//<input type="submit" /> Ignore
+/*
+echo '<center>'.form_submit(tra("Create Volunteer Account")).'</center>';
+echo '<br>';
+echo '<center><a href="http://129.114.16.64/boincserver/terms.php">Terms of Use for Volunteers.</a></center>';
+echo '<center><form action="" method="post">
+<input type="checkbox" name="agree" value="<?php echo htmlspecialchars($agree);?>"/> I hereby
+agree to the Terms of Use regarding being a Volunteer in the TACC-2-BOINC project.
+</form></center>';
+*/
+//If condition for check box
+//isset is used for true or false (if else) scenario
+//Without preset value, remains stuck in the else statement
+//With preset value, button loses responsiveness (no creating account)
+//https://stackoverflow.com/questions/37111029/php-what-does-checkbox-default-value-on
+/*
+if (isset($agree)) {
+echo '<center>'.form_submit(tra("Create Volunteer Account")).'</center>';
+}
+else {
+echo '<p>Agree to the Terms of use first.</p>';
+}
+*/
+
+//echo '<div style ="position:relative; left:36%;">'.form_submit(tra("Create Volunteer Account")).'</div>';
+
+//https://stackoverflow.com/questions/21523730/html-checkbox-submit-button-by-agreeing-on-terms-and-condition
+//<center>'.form_submit(tra("Create Volunteer Account")).'</center>;
+/*
+echo '<script>
+ function disableSubmit() {
+  document.getElementById("submit").disabled = true;
+ }
+
+  function activateButton(element) {
+
+      if(element.checked) {
+        
+        document.getElementById("submit").disabled = false;
+       }
+       else  {
+        document.getElementById("submit").disabled = true;
+      }
+
+  }
+</script>';
+*/
+//
+echo '<body onload="disableSubmit()">
+<span style = "position:relative; left:35%;">
+<a href="./terms.php">Terms of Use for Volunteers.</a>
+<br>
+ <input type="checkbox" name="terms" id="terms" onchange="activateButton(this)" >I have read and agree to the Terms of Use of being a BOINC Volunteer.
+<br><form method="post" action="">
+<br>
+  <input class="btn btn-success" type="submit" name="submit" id="submit" value="Create Volunteer Account"></form>
+  </span>';//Places the Javascript button which had to have a different position measurement than the other elements of the webpage (The button is a checkbox, once clicked allows for the creation of a Volunteer Account), Joshua assisted with class which made the button matche the rest in style
+echo "<br>";
+//--------=====---------========------------================--------------==========--------------=====-----------------====--
 form_end();
 
-page_tail();
+
+//echo '<br>';
+//echo '<center><font size=+2>'.tra("Become a Researcher:").'</font></center>'; //Code that was originally used when Thomas was separating the accounts' elements
+//Using the tool-tip script that Joshua utilized on index.php
+/*attribute href of html tag a was removed by Gerald Joshua*/
+/*
+echo '<font size=+1 style = "position:relative; left:36%;"><a data-toggle="tooltip" style="border-bottom: 1px dotted #000;text-decoration: none;" 
+//title="All active users of TACC resources are eligible to run jobs 
+//through the TACC-2-BOINC infrastructure and qualify as researchers.
+">'.tra("Become a Researcher:").'</a></font>';//Establishes the Tooltip for `Become a Researcher`, has positioning and font size alterations
+echo '<br>';//Separation for aesthetic appeal
+echo '<div>';
+echo '<span style="position:relative; left:36.335%;"><a href="https://portal.tacc.utexas.edu/account-request" class="btn btn-success"><font
+>'.tra('Create Researcher Account').'</font></a></span>';////Establishes the Tooltip for `Become a Researcher`, has positioning alterations
+*/
+ //based off of https://www.w3schools.com/tags/tryit.asp?filename=tryhtml_input_checked code
+/*echo '<form action="" method="get">
+  <input type="checkbox" name="Accept" value="Acceptance">I have read and Accept the Terms and Agreements or using TACC-2-BOINC and related BOINC software.
+</form>';
+*/
+echo '<br>';//Separation
+
+//https://stackoverflow.com/questions/21523730/html-checkbox-submit-button-by-agreeing-on-terms-and-condition
+//The script for the Javascript button
+//The button is by default disabled and only upon having the checkbox clicked does it become usable
+echo '<script>
+ function disableSubmit() {
+  document.getElementById("submit").disabled = true;
+ }
+
+  function activateButton(element) {
+
+      if(element.checked) {
+        
+        document.getElementById("submit").disabled = false;
+       }
+       else  {
+        document.getElementById("submit").disabled = true;
+      }
+
+  }
+</script>';
+echo '</div>';
+page_tail();//End of page
 
 $cvs_version_tracker[]="\$Id$";  //Generated automatically - do not edit
 ?>
--- user/info.php
+++ user/info.php
@@ -30,6 +30,7 @@ if (function_exists("project_rules_policies")) {
 }
 
 if ($show_default) {
+/*Edited by Joshua: so that the user terms and agreements align with current TACC-2-BOINC project*/
 echo "
 
 <h3>".tra("Run %1 only on authorized computers", PROJECT)."</h3>
@@ -40,9 +41,18 @@ echo "
     <p>".tra("The work done by your computer contributes to the goals of %1, as described on its web site. The application programs may change from time to time.", PROJECT)."
 
 <h3>".tra("Privacy policy")."</h3>
-    <p>".tra("Your account on %1 is identified by a name that you choose. This name may be shown on the %1 web site, along with a summary of the work your computer has done for %1. If you want to be anonymous, choose a name that doesn't reveal your identity.", PROJECT)."
+    <p>".
+    tra("We will be saving the following data related to the computing cycles donated by you: Email Address, Screen Name, Password, Country. We will be anonymizing your screen name for displaying your contributions on the leaderboard. We will notify you about the anonymized name assigned to your volunteer profile.").
+    "</p><p>".
+    tra("We guarantee the erasure of the aforementioned information from our server/s should you desire to do so after opting out as a volunteer. Please send an email at the following address to request the erasure of your data: rauta@tacc.utexas.edu").
+    "</p><p>".
+    tra("Joining the TACC-2-BOINC project requires downloading the BOINC client and completing the sign-up process on the TACC-2-BOINC website. You are welcome to adjust the default settings of the BOINC client to start and stop running it as per your desire.").
+    "</p><p>".
+	tra("By joining the TACC-2-BOINC project as a volunteer, you would be donating the computing cycles of your devices and would be footing the electricity bill associated with this donation.").
+	"</p><p>".
+	tra("The TACC-2-BOINC project team and/or the University of Texas at Austin would not be liable for any charges incurred with the aforementioned donation of computing cycles.")."</p>
     <p>".tra("If you participate in %1, information about your computer (such as its processor type, amount of memory, etc.) will be recorded by %1 and used to decide what type of work to assign to your computer. This information will also be shown on %1's web site. Nothing that reveals your computer's location (e.g. its domain name or network address) will be shown.", PROJECT)."
-    <p>".tra("To participate in %1, you must give an address where you receive email. This address will not be shown on the %1 web site or shared with organizations. %1 may send you periodic newsletters; however, you can opt out at any time.", PROJECT)."
+    <p>".tra("To participate in %1, you must give an address where you receive email. This address will not be shown on the %1 web site or shared with organizations. %1 may send you periodic messages related to your jobs; however, you can opt out at any time.", PROJECT)."
     <p>".tra("Private messages sent on the %1 web site are visible only to the sender and recipient.  %1 does not examine or police the content of private messages.  If you receive unwanted private messages from another %1 user, you may add them to your %2 message filter %3.  This will prevent you from seeing any public or private messages from that user.", PROJECT, "<a href=\"edit_forum_preferences_form.php\">", "</a>")." 
     <p>".tra("If you use our web site forums you must follow the %2 posting guidelines %3.  Messages posted to the %1 forums are visible to everyone, including non-members.  By posting to the forums, you are granting irrevocable license for anyone to view and copy your posts.", PROJECT, "<a href=moderation.php>", "</a>")." 
 <h3>".tra("Is it safe to run %1?", PROJECT)."</h3>
@@ -56,6 +66,7 @@ echo "
 <h3>".tra("Other BOINC projects")."</h3>
     <p>".tra("Other projects use the same platform, BOINC, as %1. You may want to consider participating in one or more of these projects. By doing so, your computer will do useful work even when %1 has no work available for it.", PROJECT)."
     <p>".tra("These other projects are not associated with %1, and we cannot vouch for their security practices or the nature of their research. Join them at your own risk.", PROJECT);
+/*End of the edit by Joshua*/
 }
 page_tail();
 $cvs_version_tracker[]="\$Id$";

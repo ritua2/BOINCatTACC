a8db1d4abd2c374fc09982c0878ab8a224de055c
Gerald Joshua <gerald.joshua153@utexas.edu>
2018-08-14 17:00:37 -0500

    Updated volunteers and researchers sign in pages
    Users who already signed in as a volunteer, are not allowrd to sign in as a researcher. Users who already signed in as a researcher, are not allowed to sign in as a researcher again nor as a volunteer. This way, each user will sign in as a unique identity.

--- user/login_form.php
+++ user/login_form.php
@@ -20,61 +20,72 @@ require_once("../inc/db.inc");
 require_once("../inc/util.inc");
 require_once("../inc/account.inc");
 
-check_get_args(array("next_url"));
-
-$next_url = get_str('next_url', true);
-$next_url = urldecode($next_url);
-$next_url = sanitize_local_url($next_url);
-$next_url = urlencode($next_url);
-
-$u = "login_form.php?next_url=".$next_url;
-redirect_to_secure_url($u);
-
-$user = get_logged_in_user(false);
-if ($user) {
-    page_head("Already logged in");
-    row2("You are logged in as $user->name",
-        ".  <a href=\"logout.php?".url_tokens($user->authenticator)."\">Log out</a>"
-    );
-    page_tail();
-    exit;
-}
-
 //Added by Gerald Joshua
-page_head(null, null, null, null,  null, "Log In");
+if(!isset($_SESSION))
+    session_start();
 
-echo "<br />
-	<a data-toggle='tooltip'  style='margin-left: 36%;font-size: 24px;border-bottom:1px dotted #000;text-decoration: none;' title='Those interested in supporting R&D activities of researchers and scholars using TACC resources are invited to join the TACC-2-BOINC project as volunteers and the job for volunteers are to donate their spare compute-cycles to the TACC-2-BOINC projects.'><font size=+3>Log In as a Volunteer</font></a>";//End of edit by Gerald Joshua
-echo "<br>";
-if (0) {
-echo '
-    <a href="openid_login.php?openid_iden<a href=\"create_account_form.php?next_url=$next_url\">tifier=https://www.google.com/accounts/o8/id"><img src=img/google-button.png></a>
-    <a href="openid_login.php?openid_identifier=http://yahoo.com"><img src=img/yahoo-button.png></a>
-    <br>
-';
+//Make sure that users who already log in as a researcher, can't log in as a volunteer as well
+if(!isset($_SESSION['user'])){//Added by Gerald Joshua
+    check_get_args(array("next_url"));
+
+    $next_url = get_str('next_url', true);
+    $next_url = urldecode($next_url);
+    $next_url = sanitize_local_url($next_url);
+    $next_url = urlencode($next_url);
+
+    $u = "login_form.php?next_url=".$next_url;
+    redirect_to_secure_url($u);
+
+    $user = get_logged_in_user(false);
+    if ($user) {
+        page_head("Already logged in");
+        row2("You are logged in as $user->name",
+            ".  <a href=\"logout.php?".url_tokens($user->authenticator)."\">Log out</a>"
+        );
+        page_tail();
+        exit;
+    }
+
+    //Added by Gerald Joshua
+    page_head(null, null, null, null,  null, "Log In");
+
+    echo "<br />
+    	<a data-toggle='tooltip'  style='margin-left: 36%;font-size: 24px;border-bottom:1px dotted #000;text-decoration: none;' title='Those interested in supporting R&D activities of researchers and scholars using TACC resources are invited to join the TACC-2-BOINC project as volunteers and the job for volunteers are to donate their spare compute-cycles to the TACC-2-BOINC projects.'><font size=+3>Log In as a Volunteer</font></a>";//End of edit by Gerald Joshua
+    echo "<br>";
+    if (0) {
+    echo '
+        <a href="openid_login.php?openid_iden<a href=\"create_account_form.php?next_url=$next_url\">tifier=https://www.google.com/accounts/o8/id"><img src=img/google-button.png></a>
+        <a href="openid_login.php?openid_identifier=http://yahoo.com"><img src=img/yahoo-button.png></a>
+        <br>
+    ';
+    }
+    echo "<br>";
+    login_form($next_url);
+
+    $config = get_config();
+    if (!parse_bool($config, "disable_account_creation")
+        && !parse_bool($config, "no_web_account_creation")
+    ) {
+        echo"<!-- <center><h1>Or <a href=\"create_account_form.php?next_url=$next_url\">Create an Account</h1></a></center> --><br />
+    ";
+    }
+    /*
+    //Added by Gerald Joshua, second Login button placement edited by Thomas Johnson
+    echo "<a data-toggle='tooltip'  style='margin-left: 35%;font-size: 24px;border-bottom:1px dotted #000;text-decoration: none;' title='All active users of TACC resources are eligible to run jobs through the TACC-2-BOINC infrastructure and qualify as researchers.'>Log In as a Researcher</a>
+    	<div style='margin-left: 35.45%;margin-top: 10px;'><a href='https://portal.tacc.utexas.edu/home?p_p_id=58&p_p_lifecycle=0&p_p_state=maximized&p_p_mode=view&saveLastPath=0&_58_struts_action=%2Flogin%2Flogin' class='btn btn-success'>Log In</a></div>";
+    //End of Gerald Joshua's edit
+    */
+
+    echo "
+        <script type=\"text/javascript\">
+            document.f.email_addr.focus();
+        </script>
+    ";
+} else {//Added by Gerald Joshua
+    page_head(null, null, null, null,  null, "Already Logged In");
+    echo "<center><h1>".tra("Already logged in as a researcher with username '").$_SESSION['user']."'</h1></center>";
 }
-echo "<br>";
-login_form($next_url);
-
-$config = get_config();
-if (!parse_bool($config, "disable_account_creation")
-    && !parse_bool($config, "no_web_account_creation")
-) {
-    echo"<!-- <center><h1>Or <a href=\"create_account_form.php?next_url=$next_url\">Create an Account</h1></a></center> --><br />
-";
-}
-/*
-//Added by Gerald Joshua, second Login button placement edited by Thomas Johnson
-echo "<a data-toggle='tooltip'  style='margin-left: 35%;font-size: 24px;border-bottom:1px dotted #000;text-decoration: none;' title='All active users of TACC resources are eligible to run jobs through the TACC-2-BOINC infrastructure and qualify as researchers.'>Log In as a Researcher</a>
-	<div style='margin-left: 35.45%;margin-top: 10px;'><a href='https://portal.tacc.utexas.edu/home?p_p_id=58&p_p_lifecycle=0&p_p_state=maximized&p_p_mode=view&saveLastPath=0&_58_struts_action=%2Flogin%2Flogin' class='btn btn-success'>Log In</a></div>";
-//End of Gerald Joshua's edit
-*/
-
-echo "
-    <script type=\"text/javascript\">
-        document.f.email_addr.focus();
-    </script>
-";
+//End of the edit by Gerald Joshua
 
 page_tail();
 ?>

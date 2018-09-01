9d99745558acbe23de0e5db83b4192491ab81aa5
Carlos Redondo <carlos.red@utexas.edu>
2018-09-01 13:31:06 -0500

    Created a new Redis db for job topics/tags

--- create_tag_db.py
+++ create_tag_db.py
@@ -2,11 +2,30 @@
 BASICS
 
 Creates the tag database for BOINC jobs
-This program must only be run once
+This program must only be run once; if more keys are needed, the system will automatically update
 """
 
 import sys
 import redis
+from api.topics import all_tags as at
 
 
 
+
+
+r = redis.Redis(host='0.0.0.0', port=6389, db=12)
+
+
+# Each topic (tag) can have subtags of its own, as well as a count of jobs left and a count of completed jobs
+for major in at.tags.keys():
+    A = at.tags[major]
+    B = {}
+    for minor in A:
+        B[minor] = {}
+        B[minor]["Jobs Completed"] = []
+        B[minor]["Jobs Available"] = []
+
+    B["Jobs Completed"] = []
+    B["Jobs Available"] = []
+
+    r.hmset(major, B)

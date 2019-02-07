274d95d854ae34faa55ae9bfd197f6b0e064fa3d
Ritu Arora <rauta@tacc.utexas.edu>
2019-02-07 17:26:23 -0600

    Update screen_name_anonymization_table.sql
    Added the alter command because the screen_name_anonymization table was already existing. When starting with a fresh installation, the screen_name_anonymization table should be created with three columns and not two. When this is done, the alter command would not be needed.

--- screen_name_anonymization_table.sql
+++ screen_name_anonymization_table.sql
@@ -2,3 +2,7 @@ CREATE TABLE boincserver.screen_name_anonymization (
 	name VARCHAR(254) NOT NULL,
 	random_name VARCHAR(254) NOT NULL
 );
+
+ALTER TABLE screen_name_anonymization ADD COLUMN (email_id VARCHAR (254));
+CREATE TRIGGER update_email AFTER INSERT ON user FOR EACH ROW UPDATE screen_name_anonymization SET email_id= (select email_addr from user where user.name = screen_name_anonymization.name) where email_id is NULL;
+ALTER TABLE screen_name_anonymization ADD CONSTRAINT fk_email FOREIGN KEY (email_id) REFERENCES user (email_addr) ON DELETE CASCADE ON UPDATE CASCADE;

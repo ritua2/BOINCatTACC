b88b9b6228cc87d727d0f3afea2b411ee3ce465f
Carlos Redondo <carlos.red@utexas.edu>
2019-06-26 12:42:54 -0500

    Updated signup email to account for changing username from home.php

--- api/signup_email.py
+++ api/signup_email.py
@@ -19,21 +19,14 @@ app = Flask(__name__)
 @app.route("/boincserver/v2/api/signup/volunteer/<email>/<anonym>")
 def signup_volunteer(email, anonym):
 
-    # Create the enclosing (outer) message
-    outer = {}
-    outer['email_subject'] = 'BOINC sign-up'
-    outer['user_email'] = email
-
     text = "Welcome to BOINC@TACC,\n\nThank you for registering as a volunteer! "
     text += "If you have not done so, please install the BOINC client (http://boinc.berkeley.edu/download.php) and Virtualbox (https://www.virtualbox.org/wiki/Downloads) to run BOINC@TACC jobs. "
     text += "When prompted, select BOINC@TACC from the project list or manually add the following URL: "+os.environ['URL_BASE'].replace("http://", '')+" .\n"
     text += "\nFor GDPR compliance reasons, we have created an anonymized name for you and that is: "+anonym+" . This name is "
     text += "associated with your account and you can see it in your profile settings. This anonymized named will be displayed "
-    text += "in the leaderboard on the BOINC@TACC website.For getting your actual screen name and not the anonymized name displayed on the leaderboard, please send an email to rauta@tacc.utexas.edu.\n"
+    text += "in the leaderboard on the BOINC@TACC website.For getting your actual screen name and not the anonymized name displayed on the leaderboard, please send an email to rauta@tacc.utexas.edu "
+    text += "or change it from "+os.environ['URL_BASE']+"/home.php .\n"
     text += "\n\nSincerely,\n\nThe TACC development team"
-
-    # Adds the text
-    outer["email_content"] = text
 
     # Send the email
     return ec.send_mail_complete(email, 'BOINC sign-up', text, [])

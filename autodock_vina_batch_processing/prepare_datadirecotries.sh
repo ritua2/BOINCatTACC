78fb2ec5e6b40509df0752c3096d3f6d7c1c74ea
Ritu Arora <rauta@tacc.utexas.edu>
2020-04-05 02:47:15 -0500

    Create prepare_datadirecotries.sh
    This file prepares work-units of at least 25 AutoDock-Vina commands to compare the protein/receptor (5r80_apo.pdbqt) with the PDBQT files in the list PDBQT files prepared using the find_pddqt_files_in_a_dir.sh script (e.g., AA_pdbqt.txt). The script copies sets of 25 ligands and the receptor in separate directories and prepares a compressed tar file of the directories.

---
+++ autodock_vina_batch_processing/prepare_datadirecotries.sh
@@ -0,0 +1,26 @@
+#
+#!/bin/bash
+list_of_pdqbt_files=$1
+count=$(wc -l $1| cut -d" " -f1);
+echo "Total number of PDBQT files at the provided path: $count";
+num_subdir=`expr $count / 25`;
+echo "Number of sub-directories to be created: $num_subdir";
+path_to_receptorfile=$2
+echo "Receptor file to be used: $path_to_receptorfile"
+START=1
+END=$num_subdir
+for (( i=$START; i<=$END; i++ ))
+ do
+ 	mkdir -p data_"$i";
+        cp $path_to_receptorfile data_"$i";
+j_START=$(( 25*i ));
+        j_END=25;
+        cat $list_of_pdqbt_files| head -n $j_START| tail -n $j_END > list_of_pdqbt_files_copied.txt;
+#cp list_of_pdqbt_files_copied.txt data_"$i";
+cat list_of_pdqbt_files_copied.txt | while read line
+do
+  cp $line data_"$i";
+done
+        find data_$i -type f -name "*.pdbqt" |xargs tar -czf data_$i.tgz
+rm list_of_pdqbt_files_copied.txt;
+ done

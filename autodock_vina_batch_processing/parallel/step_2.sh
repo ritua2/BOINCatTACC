8d8b18dafa4d8c5042d6ad3ac0b57a3acc97ce1f
Ritu Arora <rauta@tacc.utexas.edu>
2020-04-10 04:17:52 -0500

    Create step_2.sh

---
+++ autodock_vina_batch_processing/parallel/step_2.sh
@@ -0,0 +1,33 @@
+# Run this script as follows: ./prepare_datadirecotries.sh ./prepared_jobs/AA_pdbqt.txt ./prepared_jobs/5r80_apo.pdbqt
+#!/bin/bash
+list_of_pdqbt_files=$1
+count=$(wc -l $1| cut -d" " -f1);
+echo "Total number of PDBQT files at the provided path: $count";
+num_subdir=`expr $count / 25`;
+echo "Number of sub-directories to be created: $num_subdir";
+path_to_receptorfile=$2
+echo "Receptor file to be used: $path_to_receptorfile"
+START=1
+END=$num_subdir
+
+echo "$START" > read_from.txt
+echo "$END" >> read_from.txt
+echo "$path_to_receptorfile" >> read_from.txt
+staff.stampede2(1344)$ cat step_2.sh 
+#!/bin/bash
+
+i=$1
+list_of_pdqbt_files=$2
+path_to_receptorfile=$3
+mkdir -p data_"$i";
+cp $path_to_receptorfile data_"$i";
+j_START=$(( 25*i ));
+j_END=25;
+cat $list_of_pdqbt_files| head -n $j_START| tail -n $j_END > list_of_pdqbt_files_copied_$i.txt;
+
+cat list_of_pdqbt_files_copied_$i.txt | while read line
+        do
+            cp $line data_"$i";
+        done
+find data_$i -type f -name "*.pdbqt" |xargs tar -czf data_$i.tgz
+#rm list_of_pdqbt_files_copied_$i.txt;

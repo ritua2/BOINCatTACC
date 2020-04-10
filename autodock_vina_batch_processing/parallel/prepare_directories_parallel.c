bcb896f54bf8cbf8d7ced716c79f4904b9c27071
Ritu Arora <rauta@tacc.utexas.edu>
2020-04-10 11:35:53 -0500

    Update prepare_directories_parallel.c

--- autodock_vina_batch_processing/parallel/prepare_directories_parallel.c
+++ autodock_vina_batch_processing/parallel/prepare_directories_parallel.c
@@ -3,31 +3,32 @@
 #include <string.h>
 #include <omp.h>
 int main(int argc, char *argv[]){
-if(argc < 2){
- printf("Missing argument: please provide a script name or command that should be run on multiple cores. For example: ./prepare_directories_parallel ./hello.sh\n");
+if(argc < 3){
+ printf("Missing argument: please provide a script name or command that should be run on multiple cores. For example: ./prepare_directories_parallel test_PDBQT.txt 5r80.pdbqt\n");
  exit (-1);
 }else{
   char command[1000];
+  char argv1[100], argv2[100];
+  strcpy(argv2, argv[2]);
+  strcpy(argv1, argv[1]);
   sprintf(command, "./step_1.sh %s %s", argv[1], argv[2]);
   system(command);
   int i, start, end;
   char filename[100];
   FILE *fp = fopen("read_from.txt", "r");
   if (fp!=NULL){
-	fscanf(fp,"%d", &start);
-	fscanf(fp,"%d", &end);
+        fscanf(fp,"%d", &start);
+        fscanf(fp,"%d", &end);
         fscanf(fp,"%s", &filename);
 
   }
-  #pragma omp parallel
-  {
-    #pragma omp for
-  	for(i=start; i<=end; i++){
-   		printf("\nthread id is: %d, i is : %d\n", omp_get_thread_num(), i);
-   		sprintf(command, "./step_2.sh %d %s %s", i,argv[1], argv[2]);
-   		system(command);
-  	}
+  #pragma omp parallel for default(none) shared(start, end, argv1, argv2) private (i, command) 
+  for(i=start; i<=end; i++){
+        printf("\nthread id is: %d, i is : %d, argv1: %s, argv2: %s\n", omp_get_thread_num(), i, argv1, argv2);
+        sprintf(command, "./step_2.sh %d %s %s", i,argv1, argv2);
+        system(command);
   }
+
   fclose(fp);
 }
 return 0;

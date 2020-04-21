7cca8ddef580a0bf04af1aa7d180deb06b48776a
Ritu Arora <rauta@tacc.utexas.edu>
2020-04-21 07:23:44 -0500

    Update vina_commands.sh

--- autodock_vina_batch_processing/vina_commands.sh
+++ autodock_vina_batch_processing/vina_commands.sh
@@ -1,7 +1,7 @@
-#example showing how to run this script: ./vina_commands.sh ./prepared_jobs 5r80_apo.pdbqt 
-# Please replace "./prepared_jobs" in this script with the exact and complete path to the prepared_jobs folder
-
 #!/bin/bash
+#example showing how to run this script: ./vina_commands.sh <path-to-the-ligands-sub-directories> 5r80_apo.pdbqt 
+
+cd $1
 list_of_data_directories=$(ls -1d */)
 #echo $list_of_data_directories;
 RECEPTOR=$2
@@ -9,20 +9,21 @@ RECEPTOR=$2
 for j in $list_of_data_directories
  do
     trim_j=$(echo "${j%?}")
-    echo "tar -xvzf $trim_j.tgz;cd $trim_j;" >> "./prepared_jobs/${trim_j}_mycommands.sh"
+    echo "tar -xvzf $trim_j.tgz;cd $trim_j;" >> "$1/${trim_j}_mycommands.sh"
     cd $j;
     files=$(ls *.pdbqt);
     for i in $files
      do
        if [[ "$i" != "$RECEPTOR" ]]; then
-         echo "vina --receptor $RECEPTOR --ligand $i --center_x -27.98 --center_y 2.54 --center_z 28.15 --size_x 50 --size_y 50 --size_z 50 --out out_$i >> ${trim_j}_log.txt ;" >> "./prepared_jobs/${trim_j}_mycommands.sh"   
+         echo "vina --receptor $RECEPTOR --ligand $i --center_x 114 --center_y 121 --center_z 125 --size_x 30 --size_y 30 --size_z 30 --out out_$i >> ${trim_j}_log.txt ;" >> "$1/${trim_j}_mycommands.sh"   
        fi
      done
 
-     tr -d '\n' < "./prepared_jobs/${trim_j}_mycommands.sh" > "./prepared_jobs/${trim_j}_mycommands2.sh"
-     mv "./prepared_jobs/${trim_j}_mycommands2.sh" "./prepared_jobs/${trim_j}_mycommands.sh"
+     tr -d '\n' < "$1/${trim_j}_mycommands.sh" > "$1/${trim_j}_mycommands2.sh"
+     mv "$1/${trim_j}_mycommands2.sh" "$1/${trim_j}_mycommands.sh"
     
-     echo -en '\n'  >> "./prepared_jobs/${trim_j}_mycommands.sh"
+     echo -en '\n'  >> "$1/${trim_j}_mycommands.sh"
+     chmod +x "$1/${trim_j}_mycommands.sh"
      
     cd ..
  done

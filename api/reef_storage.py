fccdf67c0272f83d0962b0172252c5ed01b664fa
Carlos Redondo <carlos.red@utexas.edu>
2020-04-28 21:21:36 -0500

    Communication with Reef storage no longer using Redis

--- api/reef_storage.py
+++ api/reef_storage.py
@@ -10,7 +10,6 @@ import os, sys
 from flask import Flask, request, jsonify, send_file
 import preprocessing as pp
 from werkzeug.utils import secure_filename
-import redis
 import requests
 
 
@@ -80,11 +79,6 @@ def reef_upload(toktok):
 
    file = request.files['file']
 
-   assigned_allocation = float(r.get(toktok).decode('UTF-8'))
-
-   if pp.user_sandbox_size(str(toktok)) > (assigned_allocation*1073741824):
-      return 'INVALID, User has exceded asssigned allocation. Max. available allocation is '+str(assigned_allocation)+' GB'
-
    # Avoids empty filenames and those with commas
    if file.filename == '':
       return 'INVALID, no file uploaded'
@@ -98,17 +92,8 @@ def reef_upload(toktok):
 
 
 # Allows to check the user's allocation status
-@app.route('/boincserver/v2/reef_allocation_status/token=<toktok>')
-def reef_allocation_status(toktok):
-    if pp.token_test(toktok) == False:
-       return 'Invalid token'
-    used_space = pp.user_sandbox_size(str(toktok))/1073741824
-    assigned_allocation = r.get(toktok).decode('UTF-8')
-    all_info = {'Max. allocation': assigned_allocation+' GB',
-                'Used space': str(used_space)+' GB', 
-                'Space available left': str((1 - used_space/float(assigned_allocation))*100)+'% allocation available'}
+# Not necessary
 
-    return jsonify(all_info)
 
 
 # Deletes a file already present in the user

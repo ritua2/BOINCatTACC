446ca6d83b893a34e1faf0cced852534a7c8e82d
Saumya Shah <saumyashahori18@gmail.com>
2020-12-06 12:31:48 -0800

    changes to match with t2auth updates

--- autodock_vina_batch_processing/adv_automate_boinc_submit.sh
+++ autodock_vina_batch_processing/adv_automate_boinc_submit.sh
@@ -39,9 +39,14 @@ domain_name=$(dnsdomainname)
 dn=$(echo "$domain_name" | rev)
 ORK=$(echo "${dn:0:15}" | rev)
 
+# Adds the username to the database if necessary
+# Gets the actual user name
+IFS='/' read -ra unam <<< "$PWD"
+unam="${unam[3]}"
+
 
 # Validates the researcher's email against the server's API
-TOKEN=$(curl -s -F email=$userEmail -F org_key=$ORK http://$SERVER_IP:5054/boincserver/v2/api/authorize_from_org)
+TOKEN=$(curl -s -F email=$userEmail -F org_key=$ORK -F username="$unam" http://$SERVER_IP:5054/boincserver/v2/api/authorize_from_org)
 
 # Checks that the token is valid
 if [[ $TOKEN = *"INVALID"* ]]; then
@@ -50,15 +55,10 @@ if [[ $TOKEN = *"INVALID"* ]]; then
 fi
 
 # Adds the username to the database if necessary
-# Gets the actual user name
-IFS='/' read -ra unam <<< "$PWD"
-unam="${unam[3]}"
-
 # Adds the username to the database if necessary
-# Adds the username to the database if necessary
-registerUser=$(curl -s http://$SERVER_IP:5078/boincserver/v2/api/add_username/$unam/$userEmail/$TOKEN/$ORK)
+#registerUser=$(curl -s http://$SERVER_IP:5078/boincserver/v2/api/add_username/$unam/$userEmail/$TOKEN/$ORK)
 
-printf "\n${GREENGREEN}$registerUser${NCNC}\n"
+#printf "\n${GREENGREEN}$registerUser${NCNC}\n"
 
 printf "${GREENGREEN}BOINC connection established${NCNC}\n"
 
